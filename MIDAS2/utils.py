7d2b2c53d4327a252f787b43ef2165a3154796f2
tsrobinson <t.robinson7@lse.ac.uk>
2024-12-19 09:14:50 +0000

    restructure

--- MIDAS2/utils.py
+++ MIDAS2/utils.py
@@ -1,8 +1,11 @@
+import numpy as np
+
+
 def imp_mean(imputations):
-    '''
+    """
     For a generator imputation object, calculate mean of imputed values across m datasets.
-    
-    '''
-    
+
+    """
+
     imps = list(imputations)
-    return np.mean(imps, axis = 0)  
+    return np.mean(imps, axis=0)
